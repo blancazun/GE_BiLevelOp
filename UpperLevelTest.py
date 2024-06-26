ca43b60decd8a449bffdbea0b3efa6006879769a
Blanca Zuñiga <bzuniganunez@gmail.com>
2024-06-26 11:23:27 -0600

    2

--- UpperLevelTest.py
+++ UpperLevelTest.py
@@ -5,7 +5,7 @@ from pymoo.operators.crossover.pntx import TwoPointCrossover
 from pymoo.operators.mutation.bitflip import BitflipMutation
 from pymoo.operators.sampling.rnd import IntegerRandomSampling
 from pymoo.termination import get_termination
-from GE import Grammar, mapping_depth_first
+from GE import Grammar, mapping_depth_first, mapping_breadth_first, mapping_pigrammatical
 # import multiprocessing as mp
 from os.path import join
 from copy import deepcopy
@@ -24,17 +24,16 @@ class SRProblem(Problem):
         super().__init__(n_var, n_obj=1, n_constr=0, xl=xl, xu=xu, type_var=np.int_, elementwise_evaluation=False, **kwargs)
         self.grammar = deepcopy(grammar)
         self.sr_problem = deepcopy(instance)
-        self.values = np.random.permutation(n_var)
 
     def _evaluate(self, x, out, *args, **kwargs):
-        out["F"] = np.asarray([evaluate_sr(self.grammar, self.sr_problem, self.values, item) for item in x])
+        out["F"] = np.asarray([evaluate_sr(self.grammar, self.sr_problem, item) for item in x])
 
 
 if __name__ == "__main__":
     instance_path = "instances"
     instances = ["F1"]  # "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "Keijzer6"
 
-    mappings = [mapping_depth_first]
+    mappings = [mapping_depth_first]  # mapping_breadth_first, mapping_depth_first
 
     val = []
     for mapping in mappings:
@@ -42,11 +41,11 @@ if __name__ == "__main__":
             sr_train = SR(file=join(instance_path, instance, "train.csv"))
             sr_test = SR(file=join(instance_path, instance, "test.csv"))
 
-            gramatica = Grammar(mapping=mapping, file=join(instance_path, instance, "grammar.bnf"))
-            problema = SRProblem(100, xl=0.0, xu=255.0, grammar=gramatica,
+            grammarP = Grammar(mapping=mapping, file=join(instance_path, instance, "grammar.bnf"))
+            problemP = SRProblem(100, xl=0.0, xu=255.0, grammar=grammarP,
                                  instance=sr_train)
 
-            algoritmo = GA(
+            algorithm = GA(
                 pop_size=300,
                 eliminate_duplicates=True,
                 sampling=IntegerRandomSampling(),
@@ -56,15 +55,15 @@ if __name__ == "__main__":
             # ("op", OpMutation, dict(size=3))
             termination = get_termination("n_eval", 25000)  # ("n_eval", 25000) # get_termination("n_gen", 1000)
 
-            res = minimize(problema,
-                           algoritmo,
+            res = minimize(problemP,
+                           algorithm,
                            termination,
                            save_history=True,
                            verbose=False)
 
             print("-" * 40)
             print("Best solution found: \nX = %s\nF = %s" % (res.X, res.F))
-            results = gramatica.mapping(problema.values, res.X)
+            results = grammarP.mapping(res.X)
             print(mapping.__name__, instance, results)
             print(res.F, sr_test.evaluate(results))
 
